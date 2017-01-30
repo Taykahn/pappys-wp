398f985be5b7860eff9f1ed42020155465a312b3
Taykahn <marshall962000@yahoo.com>
2017-01-30 09:11:52 -0600

    Add files via upload

---
+++ Pappys-wp/footer.php
@@ -0,0 +1,21 @@
+<hr>
+
+      <footer>
+
+        <p>&copy; <?php bloginfo('name'); ?> <?php echo date('Y'); ?></p>
+
+      </footer>
+
+    </div> <!-- /container -->
+
+
+
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+
+    <?php wp_footer(); ?>
+
+  </body>
+  
+</html>
---
+++ Pappys-wp/front-page.php
@@ -0,0 +1,43 @@
+<?php get_header(); ?>
+
+    <!-- Main jumbotron for a primary marketing message or call to action -->
+    <div class="jumbotron">
+
+      <div class="container">
+        
+        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+        <?php the_content(); ?>
+
+        <?php endwhile; endif; ?>
+
+      </div><!--end container-->
+
+    </div><!--end jumbotron-->
+
+    <div class="container">
+
+      <!-- Example row of columns -->
+      <div class="row">
+
+        <div class="col-md-4">
+
+          <?php if ( dynamic_sidebar( 'front-left' ) ); ?>
+
+        </div>
+
+        <div class="col-md-4">
+
+          <?php if ( dynamic_sidebar( 'front-center' ) ); ?>
+
+        </div>
+
+        <div class="col-md-4">
+          
+          <?php if ( dynamic_sidebar( 'front-right' ) ); ?>
+
+        </div>
+
+      </div><!--end container-->
+
+<?php get_footer(); ?>
