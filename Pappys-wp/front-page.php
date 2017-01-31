a6a387c6e2490cf9229cdc3b598f05a526d40dbc
Adam Carter <adamkristopher@gmail.com>
2017-01-30 20:12:55 -0600

    Update front-page.php

--- Pappys-wp/front-page.php
+++ Pappys-wp/front-page.php
@@ -1,15 +1,23 @@
-<?php get_header(); ?>
+<?php
+/**
+ * Front page
+ */
+get_header() ?>
 
     <!-- Main jumbotron for a primary marketing message or call to action -->
     <div class="jumbotron">
 
       <div class="container">
         
-        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+        <?php if ( have_posts() ) : 
 
-        <?php the_content(); ?>
+            while ( have_posts() ) : the_post(); ?>
 
-        <?php endwhile; endif; ?>
+              <p><?php the_content() ?></p>
+
+             <?php endwhile; 
+               
+        <?php endif ?>
 
       </div><!--end container-->
 
@@ -19,7 +27,17 @@
 
       <!-- Example row of columns -->
       <div class="row">
-
+          
+        <div class="inner-content col-sm-12">
+            
+            <h1>Welcome to Pappy's Restaurant!</h1>
+            
+            <h2>We have a wide variety of entres and appitizers!</h2>
+            
+            <h3>Be sure to checkout our burger bar! Try 'em all!</h3>
+            
+        </div><!--.inner-content-->
+          
         <div class="col-md-4">
 
           <?php if ( dynamic_sidebar( 'front-left' ) ); ?>
