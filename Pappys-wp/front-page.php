0f68aa987fc13d9b18d959f7913dcd79a4d21c94
Adam Carter <adamkristopher@gmail.com>
2017-01-30 20:20:52 -0600

    Update front-page.php

--- Pappys-wp/front-page.php
+++ Pappys-wp/front-page.php
@@ -4,58 +4,39 @@
  */
 get_header() ?>
 
-    <!-- Main jumbotron for a primary marketing message or call to action -->
-    <div class="jumbotron">
+<div class="container">
+    
+  <!-- Example row of columns -->
+  <div class="row">
 
-      <div class="container">
-        
-        <?php if ( have_posts() ) : 
+    <div class="inner-content col-sm-12">
 
-            while ( have_posts() ) : the_post(); ?>
+        <h1>Welcome to Pappy's Restaurant!</h1>
 
-              <p><?php the_content() ?></p>
+        <h2>We have a wide variety of entres and appitizers!</h2>
 
-             <?php endwhile; 
-               
-        <?php endif ?>
+        <h3>Be sure to checkout our burger bar! Try 'em all!</h3>
 
-      </div><!--end container-->
+    </div><!--.inner-content-->
 
-    </div><!--end jumbotron-->
+    <div class="col-md-4">
 
-    <div class="container">
+      <?php if ( dynamic_sidebar( 'front-left' ) ); ?>
 
-      <!-- Example row of columns -->
-      <div class="row">
-          
-        <div class="inner-content col-sm-12">
-            
-            <h1>Welcome to Pappy's Restaurant!</h1>
-            
-            <h2>We have a wide variety of entres and appitizers!</h2>
-            
-            <h3>Be sure to checkout our burger bar! Try 'em all!</h3>
-            
-        </div><!--.inner-content-->
-          
-        <div class="col-md-4">
+    </div>
 
-          <?php if ( dynamic_sidebar( 'front-left' ) ); ?>
+    <div class="col-md-4">
 
-        </div>
+      <?php if ( dynamic_sidebar( 'front-center' ) ); ?>
 
-        <div class="col-md-4">
+    </div>
 
-          <?php if ( dynamic_sidebar( 'front-center' ) ); ?>
+    <div class="col-md-4">
 
-        </div>
+      <?php if ( dynamic_sidebar( 'front-right' ) ); ?>
 
-        <div class="col-md-4">
-          
-          <?php if ( dynamic_sidebar( 'front-right' ) ); ?>
+    </div>
 
-        </div>
-
-      </div><!--end container-->
+  </div><!--end container-->
 
 <?php get_footer(); ?>
