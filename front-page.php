87fcdc0425af0d63b2764688f5aed77b9b8a6e48
Taykahn <marshall962000@yahoo.com>
2017-01-31 21:47:48 -0600

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -16,8 +16,11 @@ get_header() ?>
     <div class="col-md-4">
 
       <h2>Menu Items</h2>
+        
       <img src="/images/salad-small.jpg">
+        
       <p>Pappy's has added a variety of sensational salads to choose from...</p>
+        
       <button type="button" class="btn btn-default">Our Menu</button>
 
     </div>
@@ -25,8 +28,11 @@ get_header() ?>
     <div class="col-md-4">
 
       <h2>Contact</h2>
+        
       <img src="/images/map.png">
+        
       <p>Every item on our huge menu is available for carry out.  Just call ahead at <a href="tel:8163909550">(816) 390-9550</a> and place your order.  We accept most major credit cards.</p>
+      
       <button type="button" class="btn btn-default">Our Info</button>
 
     </div>
@@ -34,8 +40,11 @@ get_header() ?>
     <div class="col-md-4">
 
       <h2>Full Bar</h2>
+        
       <img src="/images/bar2-small.jpg">
+        
       <p>Pappy's has a full bar and a wide variety of bottled and draft beer.  We offer Miller Lite, Bud Light, American Ale and Blue Moon on tap.</p>
+      
       <h3>Happy Hour every day from 5 - 8pm!</h3>
 
     </div>
